fc9643c71c128f71015a45bf98981c0ba7ba914b
Matias Reijonen <matias.reijonen73@gmail.com>
2026-02-26 10:57:35 +0200

    edited ranking

--- sql/rank-organisations-by-energyclasspoints.sql
+++ sql/rank-organisations-by-energyclasspoints.sql
@@ -1,10 +1,10 @@
- SELECT productsTable.[organisation.organisationName], COUNT(*) as occurences, AVG(energyTable.Id) as energypointsavg
+ SELECT productsTable.[organisation.organisationName], COUNT(*) as occurences, CAST(AVG(1.0 * energyTable.Id) as DECIMAL(5,2)) AS energypointsAVGoutof7
  FROM [dbo].[electronicdisplays] as productsTable
  JOIN [dbo].[EnergyClasses] as energyTable
     ON productsTable.energyClass = energyTable.energyClass
 WHERE productsTable.energyClass IS NOT NULL
 GROUP BY productsTable.[organisation.organisationName]
 HAVING COUNT(*) >= 50
-ORDER BY energypointsavg desc, occurences desc
+ORDER BY energypointsAVGoutof7 desc, occurences desc
  
  
